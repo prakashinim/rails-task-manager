b0fd761fd1d655cd2b4f6b9f4446ee8fd11706d6
Prakashini Murugan <prakashini.murugan@outlook.com>
2023-05-18 21:41:58 +0200

    tasks completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update]
+  # before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -9,28 +9,37 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
-  def show
-  end
-
   def create
     task = Task.new(task_params)
     task.save
     redirect_to task_path(task)
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
-    @task.update(set_task)
+    @task = Task.find(params[:id])
+    @task.update(task_params)
     redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path, status: :see_other
   end
 
   def task_params
     params.require(:task).permit(:title, :details)
   end
 
-  def set_task
-    @task = Task.find(params[:id])
-  end
+  # def set_task
+    # @task = Task.find(params[:id])
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'tasks#index'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   post 'tasks', to: 'tasks#create'
   get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
-  get 'tasks/:id/edit', to: 'tasks#edit'
   patch 'tasks/:id', to: 'tasks#update'
-
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
